9bef8e47b47c7c0c9b8afeed4903d15d6a0fbd18
Jack <skelton.jt9@gmail.com>
2021-11-07 17:33:49 -0500

    Added js function

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
 
-  const arrayCardNumbers = ["Ace",2,3,4,5,6,7,8,9,10,"Jack","Queen","King"];
+  const numbers = ["Ace",2,3,4,5,6,7,8,9,10,"Jack","Queen","King"];
   const arrayCardSuits = ["hearts", "spades", "diamonds", "clubs"];
 
+    arrayCardSuits.forEach(function(suit) {
+      numbers.forEach(function(number) {
+        const name = "<li>" + number + " Of " + suit + "</li>";
+        $(".deck ul").append(name);
+        // document.write(name);
+      })
+
+    
+    });
 });
